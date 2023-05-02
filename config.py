7bc37d81bfe7d1827ea7be2934e7d292c57b9d34
TGPBot <111167531+TGPBot@users.noreply.github.com>
2023-05-02 08:00:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import getenv
 
 class Config(object):
-      API_HASH = getenv("API_HASH")
-      API_ID = int(getenv("API_ID", 0))
+      API_HASH = getenv("API_HASH", "fe9a6ba19c179384e55d0ee0be216a5e")
+      API_ID = int(getenv("API_ID", 20726759))
       AS_COPY = True if getenv("AS_COPY", False) == "True" else False
-      BOT_TOKEN = getenv("BOT_TOKEN", "")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      BOT_TOKEN = getenv("BOT_TOKEN", "6058302908:AAGpTMECwmd_f-U7sPNhaMi5U0e9-3tSztc")
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001918503297").replace("\n", " ").split(' '))
